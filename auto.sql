f106d914de7ddc320042994c687e0c17ad43bf35
Ahron Sotomayor <ahronsz32@gmail.com>
2020-11-01 15:55:11 -0500

    add database auto

---
+++ auto.sql
@@ -0,0 +1,5 @@
+create database auto CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use auto;
+create table auto (id int primary key auto_increment, marca varchar(100), modelo varchar(100), color varchar(100), costo double, fecha_fabricacion datetime(6));
+
+
